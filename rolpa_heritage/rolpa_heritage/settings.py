6ceed24034eb67c67f8bd07ad6ee12d81fcae6d3
Pranish Acharya <pranishlink33@gmail.com>
2023-02-26 13:36:41 +0545

    some updates

--- rolpa_heritage/rolpa_heritage/settings.py
+++ rolpa_heritage/rolpa_heritage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_+8co)nxe#4*dc2##3tp7bsm@5mjchhkyrvzpqr*%y(o1atm2#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
